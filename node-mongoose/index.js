c696b6d44fdea82a608a9faf30afe33f35e24bb9
Shahlo Ovezniyazova <76026227+Shahlovez@users.noreply.github.com>
2022-04-06 05:50:22 -0400

    Update index.js

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,7 +8,7 @@ const connect = mongoose.connect(url, {
     useUnifiedTopology: true
 });
 
-connect.then(() => {
+    connect.then(() => {
 
         console.log('Connected correctly to server');
     
@@ -18,10 +18,25 @@ connect.then(() => {
         })
         .then(campsite => {
             console.log(campsite);
-            return Campsite.find();
+            return Campsite.findByIdAndUpdate(campsite._id, {
+                $set: { description: 'Updated Test Document' }
+            }, {
+                new: true
+            });
         })
-        .then(campsites => {
-            console.log(campsites);
+        .then(campsite => {
+            console.log(campsite);
+    
+            campsite.comments.push({
+                rating: 5,
+                text: 'What a magnificent view!',
+                author: 'Tinus Lorvaldes'
+            });
+    
+            return campsite.save();
+        })
+        .then(campsite => {
+            console.log(campsite);
             return Campsite.deleteMany();
         })
         .then(() => {
@@ -32,4 +47,3 @@ connect.then(() => {
             mongoose.connection.close();
         });
     });
-
